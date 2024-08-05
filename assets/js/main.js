8f48164bd0efb37d02aff2f866fec8d308a146ed
Samantha Arsua <samanthagwynetha@gmail.com>
2024-08-05 23:29:46 +0800

    scroll top

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,13 @@ function scrollHeader(){
 
 window.addEventListener('scroll', scrollHeader)
 
+/*===== SHOW SCROLL TOP =====*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) header.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollTop)
 
 /*===== SHOW SCROLL TOP =====*/ 
 
